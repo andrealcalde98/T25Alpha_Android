c6ecb7400f3937c801777a99bde186a2960c63b2
Christian <christiansalvador6@gmail.com>
2019-01-29 18:53:22 +0100

    ReciclerView(Hecho)(Pondre cardView en un futuro+video de on click)

--- app/src/main/java/com/example/t25alpha/Semanas/SesionesCalendario.java
+++ app/src/main/java/com/example/t25alpha/Semanas/SesionesCalendario.java
@@ -7,12 +7,23 @@ import android.support.v7.widget.RecyclerView;
 
 import com.example.t25alpha.R;
 
+import java.util.ArrayList;
+
 public class SesionesCalendario extends AppCompatActivity  {
-RecyclerView rv;
+RecyclerView recycler;
+    ArrayList<String> listaDatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sesiones_calendario);
-        rv = findViewById(R.id.rvSesiones);
+        recycler = findViewById(R.id.rvSesiones);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+
+        listaDatos = new ArrayList<String>();
+        for (int i = 1; i <8; i++){
+            listaDatos.add("Semana "+i+" ");
+        }
+        adaptadorSemanas adapter = new adaptadorSemanas(listaDatos);
+        recycler.setAdapter(adapter);
     }
 }
