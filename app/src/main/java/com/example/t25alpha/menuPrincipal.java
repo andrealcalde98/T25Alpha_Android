4b518308ecd50b6f12890fdc658a84112c209c16
Christian <christiansalvador6@gmail.com>
2019-01-29 12:26:09 +0100

    Push hasta rcycler view

--- app/src/main/java/com/example/t25alpha/menuPrincipal.java
+++ app/src/main/java/com/example/t25alpha/menuPrincipal.java
@@ -1,7 +1,11 @@
 package com.example.t25alpha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.t25alpha.Semanas.SesionesCalendario;
 
 public class menuPrincipal extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class menuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+    }
+
+    public void onClickCalendari(View view) {
+        Intent intent = new Intent(view.getContext(), SesionesCalendario.class);
+        startActivity(intent);
     }
 }
