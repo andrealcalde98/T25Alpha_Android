fd07e5b299610324a7346c507ebc409fa001e746
Christian <christiansalvador6@gmail.com>
2019-03-04 16:40:43 +0100

    Realizada la base de datos para poder acceder al login con nuestro usuario i contraseña (falta para el perfil page)

--- app/src/main/java/com/example/t25alpha/CrearUsuari.java
+++ app/src/main/java/com/example/t25alpha/CrearUsuari.java
@@ -9,20 +9,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class CrearUsuari extends AppCompatActivity {
-private EditText nom,correu,sexe,edad,altura,password;
-private Button btnRegistrar;
+    private EditText nom, correu, sexe, edad, altura, password;
+    private Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_usuari);
-        nom =(EditText)findViewById(R.id.etNombreApellidos);
-        correu =(EditText)findViewById(R.id.etCorreo);
-        sexe =(EditText)findViewById(R.id.etSexo);
-        edad=(EditText)findViewById(R.id.etEdad);
-        altura =(EditText)findViewById(R.id.etAltura);
-        password =(EditText)findViewById(R.id.etContrasena);
-        btnRegistrar =(Button)findViewById(R.id.btnRegistrarUsuari);
+        nom = (EditText) findViewById(R.id.etNombreApellidos);
+        correu = (EditText) findViewById(R.id.etCorreo);
+        sexe = (EditText) findViewById(R.id.etSexo);
+        edad = (EditText) findViewById(R.id.etEdad);
+        altura = (EditText) findViewById(R.id.etAltura);
+        password = (EditText) findViewById(R.id.etContrasena);
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrarUsuari);
 
         final BDProjecte bdprojecte = new BDProjecte(getApplicationContext());
 
@@ -32,7 +32,14 @@ private Button btnRegistrar;
                 bdprojecte.agregarCampos(nom.getText().toString(), correu.getText().toString(),
                         sexe.getText().toString(), edad.getText().toString(),
                         altura.getText().toString(), password.getText().toString());
-                        Toast.makeText(getApplicationContext(), "SE AGREGO CORRECTAMENTE", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "SE AGREGO CORRECTAMENTE", Toast.LENGTH_SHORT).show();
+
+                nom.setText("");
+                correu.setText("");
+                sexe.setText("");
+                edad.setText("");
+                altura.setText("");
+                password.setText("");
 
             }
         });
