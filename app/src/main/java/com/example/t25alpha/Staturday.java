a29594a615f8e69afc2f3e98b1220adfab00fdf5
Christian <christiansalvador6@gmail.com>
2019-03-06 13:04:11 +0100

    Canvi de colors en l'app

---
+++ app/src/main/java/com/example/t25alpha/Staturday.java
@@ -0,0 +1,13 @@
+package com.example.t25alpha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Staturday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staturday);
+    }
+}
