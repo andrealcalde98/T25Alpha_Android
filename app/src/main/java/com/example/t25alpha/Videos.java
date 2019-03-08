826fba267189d80512f6e73d411a9a95c5deda2d
papidre <andrealcalde2998@gmail.com>
2019-03-08 19:04:27 +0100

    media player video

--- app/src/main/java/com/example/t25alpha/Videos.java
+++ app/src/main/java/com/example/t25alpha/Videos.java
@@ -3,34 +3,47 @@ package com.example.t25alpha;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class Videos extends AppCompatActivity {
     VideoView video1, video2, video3, video4, video5;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
         video1 = (VideoView) findViewById(R.id.videoView1);
-        video2 = (VideoView) findViewById(R.id.videoView2);
+
+        /*btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         /*video3 = (VideoView) findViewById(R.id.videoView3);
         video4 = (VideoView) findViewById(R.id.videoView4);
         video5 = (VideoView) findViewById(R.id.videoView5);*/
 
-       String video = "http://techslides.com/demos/sample-videos/small.mp4";
+        String video = "http://techslides.com/demos/sample-videos/small.mp4";
         Uri uri = Uri.parse(video);
-        video1.setMediaController(new MediaController(getBaseContext()));
         video1.setVideoURI(uri);
-        video1.requestFocus();
-        video1.start();
+        MediaController media = new MediaController(this);
+        video1.setMediaController(media);
+        media.setAnchorView(video1);
+        /*video1.requestFocus();
+        video1.start();*/
 
-        String videoString = "http://techslides.com/demos/sample-videos/small.mp4";
+
+
+      /*  String videoString = "http://techslides.com/demos/sample-videos/small.mp4";
         Uri uri2 = Uri.parse(videoString);
         video2.setMediaController(new MediaController(this));
         video2.setVideoURI(uri2);
         video2.requestFocus();
-        video2.start();
+        video2.start();*/
     }
 }
