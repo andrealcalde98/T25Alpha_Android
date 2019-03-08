5ebdc48b8b2e896fc9721e4d96cd2338aeb6f95e
SR Salva <christiansalvador6@gmail.com>
2019-03-08 17:16:00 +0100

    Grafico con staturday boton elminado de grafico

--- app/src/main/java/com/example/t25alpha/Staturday.java
+++ app/src/main/java/com/example/t25alpha/Staturday.java
@@ -8,23 +8,36 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Staturday extends AppCompatActivity {
-    private EditText etPes;
+    private EditText etPes, etMuneca, etCintura, etTiempo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staturday);
         etPes = findViewById(R.id.etPeso);
+        etMuneca = findViewById(R.id.etMuneca);
+        etCintura = findViewById(R.id.etCintura);
+        etTiempo = findViewById(R.id.etTiempo);
     }
 
     public void onClickDatosSemanales(View view) {
         String var_peso = etPes.getText().toString();
-        if(var_peso.isEmpty()){
-            Toast.makeText(this,"No hi ha cap dada a pes",Toast.LENGTH_SHORT).show();
+        String var_muneca = etMuneca.getText().toString();
+        String var_cintura = etCintura.getText().toString();
+        String var_tiempo = etTiempo.getText().toString();
+        int var_peso_num = Integer.parseInt(etPes.getText().toString());
+        int var_muneca_num = Integer.parseInt(etMuneca.getText().toString());
+        int var_cintura_num = Integer.parseInt(etCintura.getText().toString());
+        int var_tiempo_num = Integer.parseInt(etTiempo.getText().toString());
+        if(var_peso.isEmpty() || var_muneca.isEmpty() || var_cintura.isEmpty() || var_tiempo.isEmpty()){
+            Toast.makeText(this,"NO HI HA CAP DADA A PES",Toast.LENGTH_SHORT).show();
         }
         else {
             Intent intent = new Intent(this,Grafico.class);
-            intent.putExtra("variable_peso",var_peso);
+            intent.putExtra("variable_peso",var_peso_num);
+            intent.putExtra("variable_muneca",var_muneca_num);
+            intent.putExtra("variable_cintura",var_cintura_num);
+            intent.putExtra("variable_tiempo",var_tiempo_num);
             startActivity(intent);
         }
     }
