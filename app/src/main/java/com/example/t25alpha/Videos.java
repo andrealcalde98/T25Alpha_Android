672a778eea77665536c3fdfe3664bc688119d81f
SR Salva <christiansalvador6@gmail.com>
2019-03-08 20:07:01 +0100

    Hemos acabado el apartado de los videos, ahora falta ponerlos en el tabbed page

--- app/src/main/java/com/example/t25alpha/Videos.java
+++ app/src/main/java/com/example/t25alpha/Videos.java
@@ -10,34 +10,79 @@ import android.widget.VideoView;
 
 public class Videos extends AppCompatActivity {
     VideoView video1, video2, video3, video4, video5;
-    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
         video1 = (VideoView) findViewById(R.id.videoView1);
+        video2 = (VideoView) findViewById(R.id.videoView2);
         video3 = (VideoView) findViewById(R.id.videoView3);
         video4 = (VideoView) findViewById(R.id.videoView4);
         video5 = (VideoView) findViewById(R.id.videoView5);
 
-        String path = ("android.resource://" + getPackageName() +
-                "/" + R.id.appbar);
-        Uri uri = Uri.parse(path);
+        video1();
+        video2();
+        video3();
+        video4();
+        video5();
 
-        MediaController media = new MediaController(this);
-        video1.setMediaController(media);
-        media.setAnchorView(video1);
 
 
     }
 
+    public void video1(){
+        String path = ("android.resource://" + getPackageName() +
+                "/" + R.raw.potmano);
+        Uri uri = Uri.parse(path);
+        video1.setVideoURI(uri);
+        MediaController media = new MediaController(this);
+        video1.setMediaController(media);
+        media.setAnchorView(video1);
+    }
 
+    public void video2(){
+        String path = ("android.resource://" + getPackageName() +
+                "/" + R.raw.extension);
+        Uri uri = Uri.parse(path);
+        video2.setVideoURI(uri);
+        MediaController media = new MediaController(this);
+        video2.setMediaController(media);
+        media.setAnchorView(video2);
+    }
 
+    public void video3(){
+        String path = ("android.resource://" + getPackageName() +
+                "/" + R.raw.lateral);
+        Uri uri = Uri.parse(path);
+        video3.setVideoURI(uri);
+        MediaController media = new MediaController(this);
+        video3.setMediaController(media);
+        media.setAnchorView(video3);
+    }
+
+    public void video4(){
+        String path = ("android.resource://" + getPackageName() +
+                "/" + R.raw.flexotension);
+        Uri uri = Uri.parse(path);
+        video4.setVideoURI(uri);
+        MediaController media = new MediaController(this);
+        video4.setMediaController(media);
+        media.setAnchorView(video4);
+    }
+
+    public void video5(){
+        String path = ("android.resource://" + getPackageName() +
+                "/" + R.raw.pronosupinacion);
+        Uri uri = Uri.parse(path);
+        video5.setVideoURI(uri);
+        MediaController media = new MediaController(this);
+        video5.setMediaController(media);
+        media.setAnchorView(video5);
+    }
 
         /*video1.requestFocus();
         video1.start();*/
 
-
-    }
 }
